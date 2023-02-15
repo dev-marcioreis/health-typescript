306f29941d5a6092a012503d7fec4af12b849e3a
Marcio Reis <dev.marcioreis@gmail.com>
2023-02-15 13:05:03 -0300

    remove bg

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -1,5 +1,4 @@
 import useMediaQuery from "@/hooks/mediaQuery";
-import Headers from "@/shared/Headers";
 import { SelectedPage } from "@/shared/types";
 import { HomeModernIcon, UserGroupIcon, AcademicCapIcon } from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
@@ -15,7 +14,7 @@ const Benefits = ({ setSelectedPage }: Props) => {
     <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
         <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}>
             <div>
-                <Headers>Coloque a preguiça para correr</Headers>
+                
             </div>
         </motion.div>
     </section>
